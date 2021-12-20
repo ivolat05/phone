d0e7145986707c623e05bebbc92997df0ab8b082
andrei_labadaev <53191183+ivolat05@users.noreply.github.com>
2021-12-20 08:39:53 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -62,7 +62,6 @@ openPopupButtons.forEach((button) => { // Перебираем все кнопк
 
             document.querySelector('.video__block-active').style.opacity = '0';
             document.querySelector('.video__play').play();
-            document.querySelector('.video__play').volume = 1.0;
 
 
         }
